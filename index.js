fd93850055bd8b368873f045be2840a27982afe7
Justyna Rachowicz <mrowa44@gmail.com>
2018-01-11 14:06:43 +0100

    Add "cache" XDDDDD

--- index.js
+++ index.js
@@ -18,9 +18,38 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
 
+// "cache" xD
+let cache = {};
+
+function saveCache(documentData) {
+  console.log('dupa', 'saving cache');
+  cache.documentData = documentData;
+  cache.date = new Date();
+}
+
+function getData() {
+  const cacheDate = cache.date;
+  const cacheDay = cacheDate && cacheDate.getDate();
+  const cacheMonth = cacheDate && cacheDate.getMonth();
+  const now = new Date();
+  const nowDay = now.getDate();
+  const nowMonth = now.getMonth();
+  if (cacheDay === nowDay && cacheMonth === nowMonth) {
+    console.log('dupa', 'cached data');
+    return Promise.resolve(cache.documentData);
+  } else {
+    console.log('dupa', 'request data');
+    return axios.get('https://dziekanat.ka.edu.pl/Plany/PlanyTokow/3264')
+      .then(({ data }) => {
+        saveCache(data);
+        return data;
+      });
+  }
+}
+
 app.get('/data', (req, res) => {
-  return axios.get('https://dziekanat.ka.edu.pl/Plany/PlanyTokow/3264')
-    .then(({ data }) => {
+  return getData()
+    .then((data) => {
       res.send(data);
     })
     .catch((error) => {
